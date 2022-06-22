9e04846f09139c7378ab7d77042135d13185e0ec
Kehinde <kennyadenat09@gmail.com>
2022-06-22 21:15:44 +0100

    updated components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutMainComponent } from './layout/layout-main/layout-main.component';
+import { MainComponent } from './views/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutMainComponent,
+    children: [
+      { path: '', component: MainComponent, pathMatch: 'full' },
+      // { path: 'sponsors', component: MainSponsorComponent },
+      // { path: 'exhibit', component: MainExhibitComponent },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './views/resume/resume.component';
+import { PortfolioComponent } from './views/portfolio/portfolio.component';
+import { LayoutMainComponent } from './layout/layout-main/layout-main.component';
+import { MainComponent } from './views/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    LayoutMainComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
